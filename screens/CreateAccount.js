6d72756a956f3ef94b5fec2685a5d6b67f0d86d0
MinGook <mgk8609@naver.com>
2021-11-11 14:34:05 +0900

    아폴로 시작

--- screens/CreateAccount.js
+++ screens/CreateAccount.js
@@ -21,11 +21,21 @@ export default function CreateAccount() {
   }
 
   useEffect(() => {
-    register('firstName')
-    register('lastName')
-    register('username')
-    register('email')
-    register('password')
+    register('firstName', {
+      required: true,
+    })
+    register('lastName', {
+      required: true,
+    })
+    register('username', {
+      required: true,
+    })
+    register('email', {
+      required: true,
+    })
+    register('password', {
+      required: true,
+    })
   }, [register])
   return (
     <AuthLayout>
@@ -75,8 +85,9 @@ export default function CreateAccount() {
       />
       <AuthButton
         text="Create Account"
-        disabled={true}
+        disabled={false}
         onPress={handleSubmit(onValid)}
+        loading={true}
       />
     </AuthLayout>
   )
